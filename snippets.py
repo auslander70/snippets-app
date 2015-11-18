da69056fd75089f3754e4f25b5d104229a7a648f
Wil DAmato <wil.damato@gmail.com>
2015-11-18 22:26:40 +0000

    fix indenting

--- snippets.py
+++ snippets.py
@@ -65,5 +65,5 @@ def main():
   parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
   arguments = parser.parse_args(sys.argv[1:])
   
-  if __name__ == "__main__":
-    main()
+if __name__ == "__main__":
+  main()
