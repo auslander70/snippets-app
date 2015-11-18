80884e0eb64aba59183831b30dd7c1bbfd04edd3
Wil DAmato <wil.damato@gmail.com>
2015-11-18 22:06:22 +0000

    edit and delete stubs

--- snippets.py
+++ snippets.py
@@ -16,12 +16,34 @@ def put(name, snippet):
 def get(name):
   """Retrieve the snippet with a given name.
   
-  If there no such snippet....
+  If there no such snippet, throw an error to that affect.
   
   Returns the snippet.
   """
   
   logging.error("FIXME: Unimplemented - get{!r})".format(name))
   return ""
+  
+
+def edit(name):
+  """Retrieve snippet text with a given name, allow user to replace it 
+  with new text.
+  
+  If there is no such snippet, throw an error to that affect.
+  
+  Returns the snippet.
+  """
+  
+  logging.error("FIXME: Unimplemented - edit{!r}".format(name))
+
+def delete(name):
+  """Delete snippent with a given name.
+  
+  If there is no such snippet, throw an error to that affect.
+  
+  Returns the deleted snippet.
+  """
+  
+  logging.error("FIXME: Unimplemented - delete{!r}".format(name))
   
   
